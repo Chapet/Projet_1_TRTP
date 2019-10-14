474fb3c5566006085cf51e95ff4fac9be62f22ce
hlibioulle <hadrien.libioulle@student.uclouvain.be>
2019-10-14 16:48:36 +0200

    reader() func

--- sender.c
+++ sender.c
@@ -6,19 +6,64 @@ int main(const int argc, char * argv[]) {
     }
 
     while ((opt = getopt(argc, argv, "f:")) != -1){
-        if (opt) {
-            fArg = true;
-            filename = optarg; 
+        switch (opt) {
+            case 'f' :
+                fArg = true;
+                filename = optarg;
+                break;
+            case 't' :
+                test = true;
         }
     }
+
     hostname = malloc(sizeof(char)*strlen(argv[optind]));
     char ** endptr = malloc(sizeof(char **));
     port = strtol(argv[optind+1], endptr, 10);
 
-
+    status_code ret = reader();
+    if (ret != STATUS_OK) {
+        printf("Error code : %d \n", ret);
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
 
-int sender() {
+status_code reader() {
+
+    // open filename if fArg, stdin otherwise
+    int fd = STDIN_FILENO;
+    if (fArg) {
+        fd = open(filename, O_RDONLY);
+        if (fd < 0) {
+            return E_FILENAME;
+        }
+    }
+    
+    char buf[512];
+    status_code status;
+    ssize_t nBytes = read(fd, &buf, 512);
+
+    while(nBytes > 0) {
+        status = sender(buf);
+        if (status != STATUS_OK) {
+            close(fd);
+            return status;
+        }
+        nBytes = read(fd, &buf, 512);
+    }
+
+    close(fd);
+    if (nBytes == 0) {
+        return STATUS_OK;
+    }
+    else if (nBytes < 0) {
+        return E_READING;
+    }
+    else {
+        return E_GENERIC;
+    }
+}
+
+status_code sender(char * buf) {
 
 }
--- sender.h
+++ sender.h
@@ -6,11 +6,24 @@
 #include <zlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <fcntl.h>
 #include "packet_interface.h"
+
+typedef enum {
+    STATUS_OK = 0,
+    E_GENERIC = 1,
+    E_FILENAME = 2,
+    E_READING = 3,
+    E_SENDER = 4,
+} status_code;
 
 int opt;
 bool fArg;
 char * filename;
 char * hostname;
-int port;
+long port;
+bool test = false;
+
+status_code reader();
+status_code sender();
 #endif //FORMAT_SEGMENTS_SENDER_H
