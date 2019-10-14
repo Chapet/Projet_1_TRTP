885a2c098035f1552fff63e2265442329dc83766
hlibioulle <hadrien.libioulle@student.uclouvain.be>
2019-10-14 15:52:23 +0200

    "includes" in header file and global vars declaration

--- sender.c
+++ sender.c
@@ -1,10 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <zlib.h>
-#include <string.h>
-#include <getopt.h>
-#include "packet_interface.h"
+#include "sender.h"
 
+int main(const int argc, char * argv[]) {
+    if (argc < 3 || argc > 4) {
+        return EXIT_FAILURE;
+    }
+
+    while ((opt = getopt(argc, argv, "f:")) != -1){
+        if (opt) {
+            fArg = true;
+            filename = optarg; 
+        }
+    }
+    hostname = malloc(sizeof(char)*strlen(argv[optind]));
+    char ** endptr = malloc(sizeof(char **));
+    port = strtol(argv[optind+1], endptr, 10);
+
+
+    return EXIT_SUCCESS;
+}
 
 int sender() {
 
--- sender.h
+++ sender.h
@@ -5,7 +5,12 @@
 #include <stdlib.h>
 #include <zlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "packet_interface.h"
 
-
+int opt;
+bool fArg;
+char * filename;
+char * hostname;
+int port;
 #endif //FORMAT_SEGMENTS_SENDER_H
