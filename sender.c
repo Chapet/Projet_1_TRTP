e8e7310aa22f13526bbb271a89baf397c60d2e38
hlibioulle <hadrien.libioulle@student.uclouvain.be>
2019-10-14 17:22:22 +0200

    CUnit implemented

--- sender.c
+++ sender.c
@@ -1,33 +1,5 @@
 #include "sender.h"
 
-int main(const int argc, char * argv[]) {
-    if (argc < 3 || argc > 4) {
-        return EXIT_FAILURE;
-    }
-
-    while ((opt = getopt(argc, argv, "f:")) != -1){
-        switch (opt) {
-            case 'f' :
-                fArg = true;
-                filename = optarg;
-                break;
-            case 't' :
-                test = true;
-        }
-    }
-
-    hostname = malloc(sizeof(char)*strlen(argv[optind]));
-    char ** endptr = malloc(sizeof(char **));
-    port = strtol(argv[optind+1], endptr, 10);
-
-    status_code ret = reader();
-    if (ret != STATUS_OK) {
-        printf("Error code : %d \n", ret);
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
-
 status_code reader() {
 
     // open filename if fArg, stdin otherwise
@@ -38,7 +10,7 @@ status_code reader() {
             return E_FILENAME;
         }
     }
-    
+
     char buf[512];
     status_code status;
     ssize_t nBytes = read(fd, &buf, 512);
---
+++ tests.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "../src/sender.h"
+
+int sender_setup(void) {
+    return 0;
+}
+int sender_teardown(void) {
+    return 0;
+}
+void sender_test(void) {
+    return;
+}
+
+
+int main(int argc, char *argv[]) {
+    printf("Tests are running ... \n");
+
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    // Sender
+
+    CU_pSuite pSenderSuite = NULL;
+    pSenderSuite = CU_add_suite("ma_suite", sender_setup, sender_teardown);
+    if (NULL == pSenderSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(pSenderSuite, "Test sender", sender_test))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Run tests
+    CU_basic_run_tests();
+    CU_basic_show_failures(CU_get_failure_list());
+
+    return EXIT_SUCCESS;
+}
