2a51ef471f8c29bb46986407cec70b9be25016b2
Louis <louis99laurent@gmail.com>
2019-10-14 16:52:18 +0200

    add includes

--- sender.c
+++ sender.c
@@ -1,4 +1,15 @@
 #include "sender.h"
+#include "packet_interface.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 int main(const int argc, char * argv[]) {
     if (argc < 3 || argc > 4) {
